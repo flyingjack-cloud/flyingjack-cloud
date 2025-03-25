d9828ae9866537e9cad8180df33bedaf346d01bd
Zumin Li <zumin.li.work@gmail.com>
2025-03-25 23:28:41 +0800

    Initialize project

---
+++ auth/src/test/java/top/flyingjack/uniauth/AuthApplicationTests.java
@@ -0,0 +1,13 @@
+package top.flyingjack.uniauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
